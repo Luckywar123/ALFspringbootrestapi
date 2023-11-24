c108d942aa680e2ba321d9ea3361606712d73090
Andre Luckyta Firdaus <andreluckytafirdaus2001@gmail.com>
2023-11-25 06:41:04 +0700

    Push Springboot

---
+++ src/main/java/com/juaracoding/alfspringbootrestapi/AlfspringbootrestapiApplication.java
@@ -0,0 +1,13 @@
+package com.juaracoding.alfspringbootrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlfspringbootrestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlfspringbootrestapiApplication.class, args);
+	}
+
+}
